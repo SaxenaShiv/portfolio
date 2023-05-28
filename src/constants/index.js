eda6a999e67894157b1c639c30696cbecdd9ecd9
Shiv-2712 <shivsaxena0027@gmail.com>
2023-05-28 18:33:21 +0530

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,270 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  mongodb,
+  rubyrails,
+  graphql,
+  movieApp,
+  // leaderboard,
+  dalle,
+  math,
+  movie,
+  nyeusi,
+  coursera,
+  fcC,
+  microverse,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: frontend,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'UI/UX Design',
+    icon: ux,
+  },
+  {
+    title: 'Software Prototyping',
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'Rails',
+    icon: rubyrails,
+  },
+  {
+    name: 'graphql',
+    icon: graphql,
+  },
+  {
+    name: 'mongodb',
+    icon: mongodb,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Responsive Web Design',
+    company_name: 'freeCodeCamp',
+    icon: fcC,
+    iconBg: '#333333',
+    date: 'April 2023',
+  },
+  {
+    title: 'Javascript Data Structures',
+    company_name: 'freeCodeCamp',
+    icon: fcC,
+    iconBg: '#333333',
+    date: 'April 2023',
+  },
+  {
+    title: 'Google Cloud Associate',
+    company_name: 'Coursera Inc',
+    icon: coursera,
+    iconBg: '#333333',
+    date: 'July 2022',
+  },
+  {
+    title: 'Web Development Certification',
+    company_name: 'Eduonix Learning Solutions',
+    icon: microverse,
+    iconBg: '#333333',
+    date: 'Aug 2021',
+  },
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'Movie App',
+    description: 'A movies app that displays latest movies and user is able to filter them.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: movieApp,
+    repo: 'https://github.com/Shiv-2712/IMDB_clone',
+    demo: 'https://imdbclone-by-shiv-24214b.netlify.app/',
+  },
+  {
+    id: 'project-2',
+    name: 'Dalle-E',
+    description:
+      'A dalle clone app which generates AI images on user prompt',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: dalle,
+    repo: 'https://github.com/Shiv-2712/dall-e-clone',
+    demo: 'https://dalle-clone-by-shiv.netlify.app/',
+  },
+  {
+    id: 'project-3',
+    name: 'Math Magicians',
+    description: 'This is a single-page calculator app built with React',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: math,
+    repo: 'https://github.com/Shiv-2712',
+    demo: 'https://inspiring-medovik-37d3b3.netlify.app/',
+  },
+  {
+    id: 'project-4',
+    name: 'Movie Metro',
+    description: `A single-page application that allows users to search for any movie or show's ratings and its details.`,
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: movie,
+    repo: 'https://github.com/Shiv-2712',
+    demo: 'https://movie-metro.netlify.app/',
+  },
+  {
+    id: 'project-5',
+    name: 'Nyeusi Fest Site',
+    description:
+      'This is a demo concert website for a music festival called Nyeusi.',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: nyeusi,
+    repo: 'https://github.com/Shiv-2712',
+    demo: 'https://shaqdeff.github.io/Nyeusi-Fest-Site/',
+  },
+];
+
+export { services, technologies, experiences, projects };
